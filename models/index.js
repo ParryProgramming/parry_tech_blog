e7c84c07c8701d7952f7d940c273ad653c184038
Parry <parryprogramming@gmail.com>
2024-05-31 02:36:58 -0400

    edited comment models

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment')
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+});
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+});
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+})
+
+    module.exports = {
+        User,
+        Post,
+        Comment,
+    };
